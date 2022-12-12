b4cb8a96b71a415bb252c5739111a4fae30a966d
zbohdan349 <80817223+zbohdan349@users.noreply.github.com>
2022-12-13 00:09:34 +0300

    Update http.hook.jsx

--- src/hooks/http.hook.jsx
+++ src/hooks/http.hook.jsx
@@ -13,7 +13,7 @@ export const useHttp = () => {
         headers.Authorization = token;
         
         try {
-            const response = await fetch(url,{method,body:body,headers,referrerPolicy: "unsafe_url" });
+            const response = await fetch(url,{method,body:body,headers,referrerPolicy: "unsafe-url" });
             if (!response.ok) {
                 if (response.status ===401) {
                     // auto logout if 401 Unauthorized or 403 Forbidden response returned from api
